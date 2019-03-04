fab1db5d683abe02e6373c3290cd26609e5cf687
Sittisuk <sittisuk@codefin.io>
2019-03-04 16:11:26 +0700

    install storybook

--- index.js
+++ index.js
@@ -6,5 +6,7 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import StoryBook from './storybook'
 
+// export default StoryBook
 AppRegistry.registerComponent(appName, () => App);
---
+++ storybook/stories/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import Welcome from '../../src/screen/welcome'
+import Keyboard from '../../src/component/keyboard'
+import { storyBookStore } from '../../src/redux/store'
+
+
+storiesOf('Screen', module)
+  .add('Welcome', () => storyBookStore(Welcome));
+
+storiesOf('Component', module)
+  .add('Keyboard', () => <Keyboard />);
+
