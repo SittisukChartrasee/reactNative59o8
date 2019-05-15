774cd35afd13ee5e7c828f10da33a845a2e15ca9
Nantapol <nantapol@codefin.io>
2019-05-15 18:45:34 +0700

    rick

--- src/component/lists/riskLists.js
+++ src/component/lists/riskLists.js
@@ -8,18 +8,18 @@ import colors from '../../config/colors'
 import { TLight } from '../texts'
 
 export default ({
-  title='title',
+  nameTH='title',
   color='red',
-  percent='1%'
+  weight='1%'
 }, key) => (
   <View key={key}>
     <View style={{ paddingVertical: 6, flexDirection: 'row' }}>
       <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
         <View style={{ width: 13, height: 13, borderRadius: 13/2, backgroundColor: color, marginRight: 8 }}/>
-        <TLight>{title}</TLight>
+        <TLight>{nameTH}</TLight>
       </View>
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <TLight>{percent}%</TLight>
+        <TLight>{weight}%</TLight>
       </View>
     </View>
     <View style={{ height: 1, backgroundColor: colors.smoky }}/>
