2484ebd71638f7b1051ce22397e4e6dbf1375dc9
Nantapol <nantapol@codefin.io>
2019-05-21 13:36:07 +0700

    fraud & validate career

--- src/screen/profile/career.js
+++ src/screen/profile/career.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import {
-  TouchableOpacity,
-  ScrollView,
-  Image,
-} from 'react-native'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import find from 'lodash/find'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import Screen from '../../component/screenComponent'
-import { NavBar } from '../../component/gradient'
-import { NextButton } from '../../component/button'
-import images from '../../config/images'
-import Input from '../../component/input'
-import Modal from '../../component/modal'
-import { navigateAction } from '../../redux/actions'
-import setMutation from '../../containers/mutation'
-import { updateUser } from '../../redux/actions/commonAction'
+import React from "react";
+import { TouchableOpacity, ScrollView, Image } from "react-native";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import find from "lodash/find";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import Screen from "../../component/screenComponent";
+import { NavBar } from "../../component/gradient";
+import { NextButton } from "../../component/button";
+import images from "../../config/images";
+import Input from "../../component/input";
+import Modal from "../../component/modal";
+import { navigateAction } from "../../redux/actions";
+import setMutation from "../../containers/mutation";
+import { updateUser } from "../../redux/actions/commonAction";
 
-const mapToProps = ({ user }) => ({ user })
+const mapToProps = ({ user }) => ({ user });
 const dispatchToProps = dispatch => ({
   navigateAction: bindActionCreators(navigateAction, dispatch),
   updateUser: bindActionCreators(updateUser, dispatch)
-})
+});
 
-@connect(mapToProps, dispatchToProps)
+@connect(
+  mapToProps,
+  dispatchToProps
+)
 @setMutation
 export default class extends React.Component {
   state = {
@@ -35,51 +34,66 @@ export default class extends React.Component {
     InvalidArgument: [],
     fields: [
       {
-        label: 'ประเภทธุรกิจ',
-        type: 'search',
-        field: 'busType', // isicCode
-        required: true,
-      }, {
-        label: 'อาชีพ',
-        type: 'search',
-        field: 'occupation', // occupationCode
-        required: true,
-      }, {
-        label: 'รายได้ต่อเดือน (บาท)',
-        type: 'dropdown',
+        label: "ประเภทธุรกิจ",
+        type: "search",
+        field: "busType", // isicCode
+        required: true
+      },
+      {
+        label: "อาชีพ",
+        type: "search",
+        field: "occupation", // occupationCode
+        required: true
+      },
+      {
+        label: "รายได้ต่อเดือน (บาท)",
+        type: "dropdown",
         init: [
-          { value: 'ไม่เกิน 10,000 บาท' },
-          { value: '10,001 - 20,000 บาท' },
-          { value: '20,001 - 30,000 บาท' },
-          { value: '30,001 - 50,000 บาท' },
-          { value: '50,001 - 100,000 บาท' },
-          { value: '100,001 - 200,000 บาท' },
-          { value: 'มากกว่า 200,000 บาท' },
-          { value: 'ไม่มีรายได้' }
+          { value: "ไม่เกิน 10,000 บาท" },
+          { value: "10,001 - 20,000 บาท" },
+          { value: "20,001 - 30,000 บาท" },
+          { value: "30,001 - 50,000 บาท" },
+          { value: "50,001 - 100,000 บาท" },
+          { value: "100,001 - 200,000 บาท" },
+          { value: "มากกว่า 200,000 บาท" },
+          { value: "ไม่มีรายได้" }
         ],
-        field: 'incomeRange', //incomeRangeCode
-        required: true,
-      }, {
-        label: 'ประเทศของแหล่งที่มารายได้',
-        type: 'search',
-        field: 'countrySourceOfIncome',
-        required: true,
+        field: "incomeRange", //incomeRangeCode
+        required: true
+      },
+      {
+        label: "ประเทศของแหล่งที่มารายได้",
+        type: "search",
+        field: "countrySourceOfIncome",
+        required: true
       }
     ]
-  }
+  };
 
-  handleInput = (props) => {
-    const { updateUser, user } = this.props
-    console.log(props)
+  handleInput = props => {
+    const { updateUser, user } = this.props;
+    console.log(props);
     // if (props.type === 'modal') this.setState({ modal: true }) // ใช้ทำอะไรไม่รู้
-    if (props.field === 'busType') {
-      updateUser('career', { ...user.career, [props.field]: props.value, isicCode: props.code })
-    } else if (props.field === 'busType') {
-      updateUser('career', { ...user.career, [props.field]: props.value, isicCode: props.code })
-    } else if (props.field === 'occupation') {
-      updateUser('career', { ...user.career, [props.field]: props.value, occupationCode: props.code })
-    } else if (props.field === 'incomeRange') {
-      updateUser('career', { ...user.career, [props.field]: props.value })
+    if (props.field === "busType") {
+      updateUser("career", {
+        ...user.career,
+        [props.field]: props.value,
+        isicCode: props.code
+      });
+    } else if (props.field === "busType") {
+      updateUser("career", {
+        ...user.career,
+        [props.field]: props.value,
+        isicCode: props.code
+      });
+    } else if (props.field === "occupation") {
+      updateUser("career", {
+        ...user.career,
+        [props.field]: props.value,
+        occupationCode: props.code
+      });
+    } else if (props.field === "incomeRange") {
+      updateUser("career", { ...user.career, [props.field]: props.value });
     }
 
     // ตรวจสอบความเสี่ยงแหล่งที่มาของเงิน
@@ -88,49 +102,52 @@ export default class extends React.Component {
     //   updateUser('career', { ...user.career, [props.field]: props.value, countyCode: props.code, countryRisk: props.risk })
     // }
 
-    if (props.field === 'countrySourceOfIncome') {
-      updateUser('career', { ...user.career, [props.field]: props.value, countyCode: props.code })
+    if (props.field === "countrySourceOfIncome") {
+      updateUser("career", {
+        ...user.career,
+        [props.field]: props.value,
+        countyCode: props.code
+      });
     }
+  };
 
-  }
-
-  onValidation = (field) => {
-    const { PreconditionRequired, InvalidArgument } = this.state
-    const Required = find(PreconditionRequired, (o) => {
-      if (o.field === 'isicCode' && field === 'busType') {
-        return o
+  onValidation = field => {
+    const { PreconditionRequired, InvalidArgument } = this.state;
+    const Required = find(PreconditionRequired, o => {
+      if (o.field === "isicCode" && field === "busType") {
+        return o;
       }
-      if (o.field === 'occupationCode' && field === 'occupation') {
-        return o
+      if (o.field === "occupationCode" && field === "occupation") {
+        return o;
       }
-      if (o.field === 'incomeRangeCode' && field === 'incomeRange') {
-        return o
+      if (o.field === "incomeRangeCode" && field === "incomeRange") {
+        return o;
       }
-      return o.field === field
-    })
-    const Invalid = find(InvalidArgument, (o) => {
-      if (o.field === 'isicCode' && field === 'busType') {
-        return o
+      return o.field === field;
+    });
+    const Invalid = find(InvalidArgument, o => {
+      if (o.field === "isicCode" && field === "busType") {
+        return o;
       }
-      if (o.field === 'occupationCode' && field === 'occupation') {
-        return o
+      if (o.field === "occupationCode" && field === "occupation") {
+        return o;
       }
-      if (o.field === 'incomeRangeCode' && field === 'incomeRange') {
-        return o
+      if (o.field === "incomeRangeCode" && field === "incomeRange") {
+        return o;
       }
-      return o.field === field
-    })
+      return o.field === field;
+    });
     if (Required) {
-      return Required.description
+      return Required.description;
     } else if (Invalid) {
-      return Invalid.description
+      return Invalid.description;
     }
-    return null
-  }
+    return null;
+  };
 
   onNext = async () => {
-    const { navigateAction, user } = this.props
-    await this.setState({ PreconditionRequired: [], InvalidArgument: [] })
+    const { navigateAction, user } = this.props;
+    await this.setState({ PreconditionRequired: [], InvalidArgument: [] });
     const {
       isicCode,
       occupationCode,
@@ -138,29 +155,29 @@ export default class extends React.Component {
       incomeRangeCode,
       countrySourceOfIncome,
       countyCode
-    } = user.career
-
+    } = user.career;
 
     const data = {
       isicCode,
       occupationCode,
       incomeRangeCode: incomeRange,
-      countrySourceOfIncome
-    }
+      countrySourceOfIncome: countyCode
+    };
 
-    console.log(data)
+    console.log(data);
 
-    if (countyCode === 'US') {
+    if (countyCode === "US") {
       const modal = {
         dis: `ขออภัยท่านไม่สามารถเปิดบัญชีกองทุน\nผ่านช่องทาง K-My Funds ได้\nกรุณาติดต่อ KAsset Contact Center\n02 673 3888 กด 1 และ กด 1`,
         visible: true,
         onPress: () => this.setState({ modal: { visible: false } })
-      }
-      return this.setState({ modal })
+      };
+      return this.setState({ modal });
     } else {
-      this.props.saveCareer({ variables: { input: data } })
+      this.props
+        .saveCareer({ variables: { input: data } })
         .then(res => {
-          console.log(res)
+          console.log(res);
 
           // ตรวจสอบความเสี่ยงของประเทศ
 
@@ -174,32 +191,36 @@ export default class extends React.Component {
           // } else if (res.data.saveCareer.success) {
 
           if (res.data.saveCareer.success) {
-            navigateAction({ ...this.props, page: 'sourceOfFund' })
+            navigateAction({ ...this.props, page: "sourceOfFund" });
           } else if (!res.data.saveCareer.success) {
             switch (res.data.saveCareer.message) {
-              case 'PreconditionRequired':
-                return this.setState({ PreconditionRequired: res.data.saveCareer.details })
-              case 'InvalidArgument':
-                return this.setState({ InvalidArgument: res.data.saveCareer.details })
+              case "PreconditionRequired":
+                return this.setState({
+                  PreconditionRequired: res.data.saveCareer.details
+                });
+              case "InvalidArgument":
+                return this.setState({
+                  InvalidArgument: res.data.saveCareer.details
+                });
               default:
                 const modal = {
                   dis: res.data.saveCareer.message,
                   visible: true,
                   onPress: () => this.setState({ modal: { visible: false } })
-                }
-                return this.setState({ modal })
+                };
+                return this.setState({ modal });
             }
           }
         })
         .catch(err => {
-          console.log(err)
-        })
+          console.log(err);
+        });
     }
-  }
+  };
 
   render() {
-    const { user } = this.props
-    const { modal } = this.state
+    const { user } = this.props;
+    const { modal } = this.state;
     return (
       <Screen color="transparent">
         <NavBar
@@ -222,27 +243,28 @@ export default class extends React.Component {
           contentContainerStyle={{ paddingBottom: 100 }}
           showsVerticalScrollIndicator={false}
         >
-          {
-            this.state.fields.map((d, key) => Input({
-              field: d.field,
-              label: d.label,
-              type: d.type,
-              required: d.required,
-              init: d.init,
-              value: user.career[d.field],
-              inVisible: d.inVisible,
-              handleInput: (props) => this.handleInput(props),
-              err: this.onValidation(d.field),
-            }, key))
-          }
+          {this.state.fields.map((d, key) =>
+            Input(
+              {
+                field: d.field,
+                label: d.label,
+                type: d.type,
+                required: d.required,
+                init: d.init,
+                value: user.career[d.field],
+                inVisible: d.inVisible,
+                handleInput: props => this.handleInput(props),
+                err: this.onValidation(d.field)
+              },
+              key
+            )
+          )}
         </KeyboardAwareScrollView>
 
-        {
-          Modal(modal)
-        }
+        {Modal(modal)}
 
         <NextButton onPress={this.onNext} />
       </Screen>
-    )
+    );
   }
 }
