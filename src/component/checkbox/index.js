9a064b4fad05ed824c4e1440d37b0e0a2b710b05
Sittisuk <sittisuk@codefin.io>
2019-06-05 12:13:03 +0700

    handle modal lockout

---
+++ src/component/checkbox/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native'
+import images from '../../config/images';
+import colors from '../../config/colors';
+
+export default class extends React.Component {
+  static defaultProps = {
+    value: true,
+    onChange: () => alert('neet function onChange!')
+  }
+  state = {
+    value: this.props.value,
+  }
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          this.setState({ value: !this.state.value }, () => {
+            this.props.onChange(this.state.value)
+          })
+          
+        }}
+        style={{ padding: 16 }}
+        activeOpacity={1}
+      >
+        {
+          this.state.value
+            ? <Image source={images.iconCheck} style={{ width: 16, height: 16, borderRadius: 5, backgroundColor: 'black' }} />
+            : <Image source={{}} style={{ width: 16, height: 16, borderRadius: 5, borderWidth: 1, borderColor: colors.smoky }} />
+        }
+      </TouchableOpacity>
+    )
+  }
+}
--- src/component/modal/index.js
+++ src/component/modal/index.js
@@ -19,45 +19,83 @@ import {
   NextButton,
   FlexButton,
 } from '../button'
+import CheckBox from '../checkbox'
+import colors from '../../config/colors';
 
-export default ({
-  visible=true,
-  image,
-  type,
-  labelBtn="ตกลง",
-  dis="รหัส OTP ไม่ถูกต้อง \nคุณสามารถกรอกได้อีก n ครั้ง",
-  onPress=() => alert('use func onPress yet!'),
-  onConfirm=() => alert('use func onConfirm yet!'),
-  onPressClose=() => alert('use func onPressClose yet!'),
-  disabled=false
-}) => (
-  <Modal isVisible={visible} onSwipeComplete={onPressClose} swipeDirection="left" useNativeDriver={true}>
-    <TouchableOpacity 
-      onPress={onPressClose}
-      activeOpacity={1}
-      disabled={disabled}
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}
-    >
-      <TouchableOpacity
-        activeOpacity={1}
-        style={{ flex: 1, padding: 24, backgroundColor: 'white', borderRadius: 8 }}
-      >
-        { image && <Image source={image} style={{ alignSelf: 'center', marginBottom: 24 }} /> }
-        <TLight>{dis}</TLight>
+export default class extends React.Component {
+  static defaultProps = {
+    visible: true,
+    image: undefined,
+    type: undefined,
+    labelBtn: "ตกลง",
+    dis: "รหัส OTP ไม่ถูกต้อง \nคุณสามารถกรอกได้อีก n ครั้ง",
+    onChange: undefined,
+    onPress: () => alert('use func onPress yet!'),
+    onConfirm: () => alert('use func onConfirm yet!'),
+    onPressClose: () => alert('use func onPressClose yet!'),
+    disabled: false
+  }
 
-        {
-          type
-            ? (
-              <View style={{ flexDirection: 'row' }}>
-                <FlexButton border onPress={onConfirm}>ใช่</FlexButton>
-                <FlexButton marginLeft={16} onPress={onPressClose}>ไม่ใช่</FlexButton>
+  render() {
+    const {
+      visible,
+      image,
+      type,
+      labelBtn,
+      dis,
+      onPress,
+      onChange,
+      onConfirm,
+      onPressClose,
+      disabled
+    } = this.props
+
+    // console.log(onChange)
+    return (
+      <Modal isVisible={visible} onSwipeComplete={onPressClose} swipeDirection="left" useNativeDriver={true}>
+        <TouchableOpacity 
+          onPress={onPressClose}
+          activeOpacity={1}
+          disabled={disabled}
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}
+        >
+          <TouchableOpacity
+            activeOpacity={1}
+            style={{ flex: 1, padding: 24, backgroundColor: 'white', borderRadius: 8 }}
+          >
+            { image && <Image source={image} style={{ alignSelf: 'center', marginBottom: 24 }} /> }
+            <TLight>{dis}</TLight>
+            { onChange && (
+              <View 
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderWidth: 1,
+                  borderRadius: 10,
+                  borderColor: colors.smoky,
+                  marginTop: 16,
+              }}>
+                <CheckBox onChange={val => onChange(val)} />
+                <TLight>ไม่ต้องการบันทึกข้อมูล</TLight>
               </View>
-            ) : (
-              <LongButton label={labelBtn} onPress={onPress} />
-            )
-        }
-        
-      </TouchableOpacity>
-    </TouchableOpacity>
-  </Modal>
-)
+            )}
+
+            {
+              type
+                ? (
+                  <View style={{ flexDirection: 'row' }}>
+                    <FlexButton border onPress={onConfirm}>ใช่</FlexButton>
+                    <FlexButton marginLeft={16} onPress={onPressClose}>ไม่ใช่</FlexButton>
+                  </View>
+                ) : (
+                  <LongButton label={labelBtn} onPress={onPress} />
+                )
+            }
+            
+          </TouchableOpacity>
+        </TouchableOpacity>
+      </Modal>
+    )
+  }
+}
