cabd37d06bd5387586ddb5009ca3ce5e55ecf087
Sittisuk <sittisuk@codefin.io>
2019-07-24 18:45:02 +0700

    handle bug border undified in IOS native

--- src/screen/complete/confirmAccount.js
+++ src/screen/complete/confirmAccount.js
@@ -149,14 +149,16 @@ export default class extends React.Component {
 									agree
 										? <Image source={images.iconCheck}
 											style={{ width: 16, height: 16, borderRadius: 5, backgroundColor: 'red' }} />
-										: <Image source={{}} style={{
-											width: 16,
-											height: 16,
-											borderRadius: 5,
-											backgroundColor: colors.white,
-											borderWidth: 1,
-											borderColor: colors.grey
-										}} />
+										: <View
+												style={{
+													width: 16,
+													height: 16,
+													borderRadius: 5,
+													backgroundColor: colors.white,
+													borderWidth: 1,
+													borderColor: colors.grey
+												}} 
+											/>
 								}
 							</View>
 							<View style={{ flex: 1 }}>
