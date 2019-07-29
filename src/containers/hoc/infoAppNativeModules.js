64f78c844f7aee723bb211d8039cd8840c57312f
Nantapol <nantapol@codefin.io>
2019-07-29 11:02:50 +0700

    Build 44 UAT

--- src/containers/hoc/infoAppNativeModules.js
+++ src/containers/hoc/infoAppNativeModules.js
@@ -10,18 +10,15 @@ export default (ComponentWrapper) => {
   class Enhance extends React.Component {
     state = {
       fcm: '',
-      fcm2: '',
       version: '',
     }
     componentDidMount = () => {
       if (NativeModules.KMyFundOnboarding) {
-        NativeModules.KMyFundOnboarding.getFCMToken((fcm, fcm2) => {
-          NativeModules.KMyFundOnboarding.getVersionAppKMyFunds((version, version2) => {
+        NativeModules.KMyFundOnboarding.getFCMToken(fcm => {
+          NativeModules.KMyFundOnboarding.getVersionAppKMyFunds(version => {
             this.setState({
               fcm,
-              fcm2,
               version,
-              version2
             })
           })
         })
@@ -29,20 +26,8 @@ export default (ComponentWrapper) => {
     }
 
     render() {
-<<<<<<< HEAD
-      const { fcm, fcm2, version, version2 } = this.state
-      const FCMtoken2 = 'Test for FCM IOS'
-      alert(FCMtoken2)
-      return <ComponentWrapper
-        {...this.props}
-        fcm={Platform === 'android' ? fcm : FCMtoken2}
-        version={Platform === 'android' ? version : version2}
-
-      />
-=======
       const { fcm, version } = this.state
       return <ComponentWrapper {...this.props} fcm={fcm} version={version} />
->>>>>>> b97d12c9f7b27c1a04b8798c44789ad898383fae
     }
   }
 
