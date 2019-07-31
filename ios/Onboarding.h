cda18f171c8d0623f91f1574df501f3449725ac0
Sittisuk <sittisuk@codefin.io>
2019-07-31 16:31:32 +0700

    update class Onboarding.h & .m

---
+++ ios/Onboarding.h
@@ -0,0 +1,22 @@
+//
+//  KMyFundOnboarding.h
+//  KbankOnboarding
+//
+//  Created by codefin3 on 7/31/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+//#ifndef KMyFundOnboarding_h
+
+#import <React/RCTBridgeModule.h>
+
+@interface KMyFundOnboarding : NSObject <RCTBridgeModule>
+@end
+
+//#define KMyFundOnboarding_h
+//
+//
+//
+//#endif /* KMyFundOnboarding_h */
+
+
---
+++ ios/Onboarding.m
@@ -0,0 +1,19 @@
+//
+//  KMyFundOnboarding.m
+//  KbankOnboarding
+//
+//  Created by codefin3 on 7/31/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "KMyFundOnboarding.h"
+
+@implementation KMyFundOnboarding
+  RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+  {
+    NSLog(@"%s", "beer");
+  }
+
+@end
+
+
