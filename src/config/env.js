37f71f7aee02669aadf744e67a058f4aaf70ad56
Sittisuk <sittisuk@codefin.io>
2019-08-02 14:48:02 +0700

    edit env

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 export default {
-  SIT: 'https://k-myfunds.kasikornasset.com:30443',
   DEV: 'https://ka-ob-client-dev.codefin.io',
+  SIT: 'https://k-myfunds.kasikornasset.com:30443',
   UAT: 'https://k-myfunds.kasikornasset.com:31443',
   PRO: 'https://k-myfunds.kasikornasset.com:32443',
 }
