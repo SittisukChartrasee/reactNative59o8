a392b292e4689531686f5072498fc2038b495851
sittisuk <sittisuk@codefin.io>
2020-03-24 16:51:11 +0700

    init project

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
-import StoryBook from './storybook'
 
-if (__DEV__) {
-  global.XMLHttpRequest = global.originalXMLHttpRequest ?
-        global.originalXMLHttpRequest :
-        global.XMLHttpRequest;
-  global.FormData = global.originalFormData ?
-      global.originalFormData :
-      global.FormData;
-  global.Blob = global.originalBlob ?
-      global.originalBlob :
-      global.Blob;
-  global.FileReader = global.originalFileReader ?
-      global.originalFileReader :
-      global.FileReader;
-}
-
-export default StoryBook
 AppRegistry.registerComponent(appName, () => App);
--- src/App.js
+++ src/App.js
@@ -1,115 +1,13 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { NavigationActions } from 'react-navigation'
-import { Text, View, AppState, BackHandler, NativeModules } from 'react-native'
-import { createReduxContainer } from 'react-navigation-redux-helpers'
-import { bindActionCreators } from 'redux'
-import { onStore, AppNavigator } from './redux/store'
-import provider from './config/provider'
-import Modal from './component/modal'
-import Screen from './component/screenComponent'
-import ScreenModal from './component/screenModal'
-import { root } from './redux/actions/commonAction'
-import colors from './config/colors';
-import { TBold } from './component/texts';
-import Spinner from './component/spinner'
-import releaseApp from '../release/releaseApp.json'
-import SecureKeyStore from "../src/utility/keyStore"
-import hardwareBackPress from './containers/hoc/hardwareBackPress'
+import {
+  View,
+} from 'react-native'
+import { TBold } from './component/texts'
 
-export const store = onStore
-const ReactWithState = connect(({ nav }) => ({ state: nav }))(createReduxContainer(AppNavigator, 'root'))
-
-const TEST = false // ถ้าจะ TEST ให้ set true : false
-
-const mapToProps = ({ root, nav, bank }) => ({ root, nav, bank })
-const dispatchToProps = dispatch => ({
-  updateRoot: bindActionCreators(root, dispatch),
-  handleScreen: (value) => dispatch({ type: 'CHECKSCREEN', value }),
-})
-
-@provider
-@connect(mapToProps, dispatchToProps)
-@hardwareBackPress
-export default class extends React.Component {
-  constructor(props) {
-    super(props)
-
-    Text.defaultProps = Text.defaultProps || {};
-    Text.defaultProps.allowFontScaling = false;
-
-    this.state = {
-      appState: AppState.currentState,
-      loading: false,
-    }
-
-    !TEST &&
-      SecureKeyStore.get("user_token")
-        .then(async d => {
-          const a = await SecureKeyStore.get('userToken')
-          console.log(a, d)
-          if (d) {
-            this.setState({ loading: true })
-            this.props.handleScreen('login')
-          } else {
-            this.setState({ loading: true })
-            this.props.handleScreen('firstTerm')
-          }
-        })
-  }
-
-
-
-  componentDidMount() {
-    AppState.addEventListener('change', this._handleAppStateChange);
-    this._handleGetBundleVersion(releaseApp)
-
-    // const nativeEventListener = DeviceEventEmitter.addListener('ActivityStateChange',
-    //   (e)=>{
-    //       console.log(e.event);
-    //       // this.setState({ test: e.event })
-
-    //       if (e.event === 'inactive') return ToastAndroid.show('bye bye', ToastAndroid.SHORT)
-    //       else if (e.event === 'active') return ToastAndroid.show('Welcome', ToastAndroid.SHORT)
-    // })
-  }
-
-  componentWillUnmount() {
-    AppState.removeEventListener('change', this._handleAppStateChange);
-  }
-
-  _handleGetBundleVersion = release => NativeModules.KMyFundOnboarding.getBundleVersion(`${release.nameFile}, ENV: ${release.env}, Build: ${release.build}`)
-
-  _handleAppStateChange = (nextAppState) => {
-    if (
-      this.state.appState.match(/inactive|background/) &&
-      nextAppState === 'active'
-    ) {
-      console.log('App has come to the foreground!');
-    }
-    this.setState({ appState: nextAppState });
-    this.props.updateRoot('appState', nextAppState)
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        {
-          !TEST ?
-            this.state.loading
-              ? <ReactWithState />
-              : <Screen />
-            : <ReactWithState />
-        }
-
-        {
-          this.props.root.screenModal.visible ?
-            <ScreenModal modal={this.props.root.modal} {...this.props.root.screenModal} /> :
-            <Modal {...this.props.root.modal} />
-        }
-
-        <Spinner loading={this.props.root.loading} />
-      </View>
-    )
-  }
+export default () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TBold fontSize="30px">{`Welcome To React Native 059.8`}</TBold>
+    </View>
+  )
 }
