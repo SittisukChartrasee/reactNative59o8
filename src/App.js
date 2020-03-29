5201cb5cc7f25643f18b375728ada98cd6279b4c
sittisuk <sittisuk@codefin.io>
2020-03-29 21:39:50 +0700

    add react-native-dragon-libs and setup ios

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {
   View,
 } from 'react-native'
-import { TBold } from './component/texts'
+import { TBold, TText } from './component/texts'
+import DragonLibs from 'react-native-dragon-libs'
 
 export default () => {
+  DragonLibs.sampleMethod('aa', 22, val => console.log(val))
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
       <TBold fontSize="30px">{`Welcome To React Native 059.8`}</TBold>
     </View>
   )
