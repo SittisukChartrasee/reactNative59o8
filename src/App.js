d3273b48fcc491ccfa8d75943adb8872f5b6602a
sittisuk <sittisuk@codefin.io>
2020-03-30 10:04:05 +0700

    install lib react-native and lib

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import {
 } from 'react-native'
 import { TBold, TText } from './component/texts'
 import DragonLibs from 'react-native-dragon-libs'
+import Dragon from 'dragonlibs'
 
 export default () => {
   DragonLibs.sampleMethod('aa', 22, val => console.log(val))
+  console.log(Dragon('aasdfasdfa'))
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
       <TBold fontSize="30px">{`Welcome To React Native 059.8`}</TBold>
