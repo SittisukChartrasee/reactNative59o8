5ad11d227e51a9ad2d3b56d79284744de99510ab
sittisuk <sittisuk@codefin.io>
2020-07-06 15:24:32 +0700

    build to xamarin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   View,
+  Button
 } from 'react-native'
 import { TBold, TText } from './component/texts'
-import DragonLibs from 'react-native-dragon-libs'
-import Dragon from 'dragonlibs'
+// import DragonLibs from 'react-native-dragon-libs'
+// import Dragon from 'dragonlibs'
 
 export default () => {
-  DragonLibs.sampleMethod('aa', 22, val => console.log(val))
-  console.log(Dragon('aasdfasdfa'))
+  const [count, setCount] = useState(0)
+  // DragonLibs.sampleMethod('aa', 22, val => console.log(val))
+  // console.log(Dragon('aasdfasdfa'))
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
-      <TBold fontSize="30px">{`Welcome To React Native 059.8`}</TBold>
+      <TBold fontSize="30px">{`Welcome To React Native 059.8`} = {count}</TBold>
+      <Button title="Counter + " onPress={() => setCount(count + 1)} />
     </View>
   )
 }
